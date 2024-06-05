0dc34cd9d6a0a2577f372ddfb9274a80289d3149
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-06-05 01:32:33 -0700

    Created new State as well as handler function to update State when adding new project. Replaced content with variable which conditionally shows either NewProject or NoProjectSelected and forwarded handler function to Sidebar as well as NoProjectSelected to toggle content.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
+import { useState } from 'react';
+
 import ProjectsSidebar from './components/ProjectsSidebar.jsx';
 import NewProject from './components/NewProject.jsx';
 import NoProjectSelected from './components/NoProjectSelected.jsx';
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null
+      }
+    });
+  }
+
+  let content;
+
+  // STATE
+  // undefined: we are doing nothing
+  // null: we are adding a project
+  // id: show specific project
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined ) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
-      <NoProjectSelected />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
