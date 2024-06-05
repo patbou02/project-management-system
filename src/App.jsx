9ad43e54ee0b39a5007a4b6412c9983a833ccec2
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-06-04 21:52:29 -0700

    Created the ProjectsSidebar component and gave it some basic structure and styling. Imported it into App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import ProjectsSidebar from './components/ProjectsSidebar.jsx';
+
 function App() {
   return (
-    <>
+    <main className="h-screen my-8">
       <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+      <ProjectsSidebar />
+    </main>
   );
 }
 
