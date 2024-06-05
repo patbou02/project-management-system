b635c7ddcf69c60e57de0bffe75c2cf30300eb53
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-06-04 23:03:32 -0700

    Created NoProjectSelected component as well as Button component. Then imported Button in the NewProject, NoProjectSelected and ProjectSidebar components. Added NoProjectSelected component to the App component for testing.

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,4 +1,5 @@
 import Input from './Input.jsx';
+import Button from './Button.jsx';
 
 export default function NewProject() {
   return (
@@ -8,7 +9,7 @@ export default function NewProject() {
           <button className="text-stone-800 hover:text-stone-950">Cancel</button>
         </li>
         <li>
-          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button>
+          <Button>Save</Button>
         </li>
       </menu>
       <div>
