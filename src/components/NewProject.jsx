9620e3734c2eb56f23d90af0250f91f191fda511
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-06-05 23:22:32 -0700

    With the ref values available for title, description and due date, setup a handleSave() method which stores each entered value in separate variables. Then added a onAdd argument which is then fired inside the handleSave() method and which stores each piece of data as part of an Object. Fire that handleSave() method on click of the save button.

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -3,10 +3,26 @@ import { useRef } from 'react';
 import Input from './Input.jsx';
 import Button from './Button.jsx';
 
-export default function NewProject() {
+export default function NewProject({ onAdd }) {
   const title      = useRef();
   const description= useRef();
   const dueDate    = useRef();
+
+  const handleSave = () => {
+    const enteredTitle       = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate     = dueDate.current.value;
+
+    // validation
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    });
+  };
+
+
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
@@ -14,7 +30,7 @@ export default function NewProject() {
           <button className="text-stone-800 hover:text-stone-950">Cancel</button>
         </li>
         <li>
-          <Button>Save</Button>
+          <Button onClick={handleSave}>Save</Button>
         </li>
       </menu>
       <div>
