a548ab71736778c201a256d31f7359e95b2d570a
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-06-09 16:54:40 -0700

    Added handleDeleteProject method which updates the state with a new array and sets the selectedProjectId to undefined and pass that down to the ProjectDetails component using the onDelete new prop.

--- src/App.jsx
+++ src/App.jsx
@@ -55,9 +55,19 @@ function App() {
     );
   }
 
+  function handleDeleteProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(project => project.id !== prevState.selectedProjectId)
+      };
+    });
+  }
+
   const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
 
-  let content = <ProjectDetails project={selectedProject} />;
+  let content = <ProjectDetails project={selectedProject} onDelete={handleDeleteProject} />;
 
   // STATE
   // undefined: we are doing nothing
--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -1,4 +1,4 @@
-export default function ProjectDetails({ project }) {
+export default function ProjectDetails({ project, onDelete }) {
 
   const dateFormatOptions = { year: 'numeric', month: 'short', day: 'numeric' };
   const formattedDate = new Date(project.date).toLocaleDateString('en-US', dateFormatOptions);
@@ -8,7 +8,7 @@ export default function ProjectDetails({ project }) {
       <header className="pb-4 mb-4 border-b-2 border-stone-300">
         <div className="flex items-center justify-between">
           <h1 className="text-3xl font-bold text-stone-600 mb-2">{project.title}</h1>
-          <button className="text-stone-600 hover:text-stone-950">Delete</button>
+          <button className="text-stone-600 hover:text-stone-950" onClick={onDelete}>Delete</button>
         </div>
         <p className="mb-4 text-stone-400">{formattedDate}</p>
         <p className="text-stone-600 whitespace-pre-wrap">{project.description}</p>
