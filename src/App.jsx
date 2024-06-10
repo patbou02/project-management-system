020577caae990f2d3bd2b2bbfd461ab3b941102e
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-06-09 22:20:22 -0700

    In App component added a tasks array to the projectState State as well as a handleAddTask and handleDeleteTask functions where the add function adds a task to the State's current tasks array.

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,27 @@ import ProjectDetails from './components/ProjectDetails.jsx';
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
-    projects: []
+    projects: [],
+    tasks: []
   });
+
+  function handleAddTask(text) {
+    setProjectsState(prevState => {
+      const taskId = Math.random();
+      const newTask = {
+        text: text,
+        projectId: prevState.selectedProjectId,
+        id: taskId
+      };
+
+      return {
+        ...prevState,
+        tasks: [...prevState.tasks, newTask]
+      };
+    });
+  }
+
+  function handleDeleteTask() {}
 
   function handleStartAddProject() {
     setProjectsState(prevState => {
